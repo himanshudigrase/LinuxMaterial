465a0fd15e1601ba924979cf558111e50baf66d8
himanshudigrase <42116677+himanshudigrase@users.noreply.github.com>
2022-09-13 22:50:06 +0530

    Update 1b.c

--- 1b.c
+++ 1b.c
@@ -9,6 +9,10 @@
 
 int main(int argc, char* argv[]){
 
+  //If we create a hard link to the file and then delete the file, we can still access the file using hard link. 
+  //But if we create a soft link of the file and then delete the file, we can’t access the file through soft link and soft link becomes dangling.
+  //Basically hard link increases reference count of a location while soft links work as a shortcu
+  
   int l = link(argv[1],argv[2]);
 
   if(l == 0){
