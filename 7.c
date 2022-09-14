3a2ea80060a8444bb06ce7797e23850d5c42473d
himanshudigrase <42116677+himanshudigrase@users.noreply.github.com>
2022-09-14 15:46:02 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -19,9 +19,20 @@ void main(int argc, char **argv){
  else printf("\n File opened for copy");
 
  fp2 = fopen(argv[2],"w");
+ 
+ //fseek() function sets the file position indicator for the
+ // stream pointed to by stream
+ 
+ // In the below command pointer is pointing to eof and moving 0 dist from it.
+ 
  fseek(fp1,0L,SEEK_END);
+ 
+ //ftell() function obtains the current value of the file
+      // position indicator for the stream pointed to by stream
+ 
  pos = ftell(fp1);
  fseek(fp1,0L,SEEK_SET);
+ // seek_set points to start of file
 
  while(pos--){
   ch = fgetc(fp1);
