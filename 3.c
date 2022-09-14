d618fd12294966cb2c95611cff2417a3626f0f08
himanshudigrase <42116677+himanshudigrase@users.noreply.github.com>
2022-09-14 15:01:33 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -7,7 +7,7 @@
 #include<fcntl.h>
 
 void main(){
-
+// creates a new file and gives r,w,x permission to user and r to others and grp 
  int fd = creat("a.txt",S_IRUSR|S_IRWXU|S_IRGRP|S_IROTH);
  printf("fd = %d\n",fd);
 }
