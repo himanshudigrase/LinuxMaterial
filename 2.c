80b36feb07717d53e4ec6781f121515d4f91a29a
himanshudigrase <42116677+himanshudigrase@users.noreply.github.com>
2022-09-14 14:52:01 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -15,3 +15,7 @@ while(1)
 return 0;
 }
 
+//after running this program compile it and run ./a.out &, which will give us process id
+//Now go to /proc directory and ls and check whether our process is is present or not.
+	//if yes then cat /proc/id/status which will give us status and info about our process
+
