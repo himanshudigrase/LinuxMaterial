806034a7d1d8c4c81d11ef65c998171ec397fa8a
himanshudigrase <42116677+himanshudigrase@users.noreply.github.com>
2022-09-15 19:00:12 +0530

    Update 23.c

--- 23.c
+++ 23.c
@@ -4,6 +4,14 @@
 // Question : Write a program to create a Zombie state of the running program
 
 
+// ZOmbie processes that have completed their execution, but their entries are not removed from the process table
+//If the parent decides not to wait for the child's termination and executes its subsequent task, then at the termination of the child, the exit status is not read.
+//Hence, there remains an entry in the process table even after the termination of the child. This state of the child process is known as the Zombie state.
+//ps -eaf check process table for xombie process
+// ./a.out defunct shows that it is a zombie process
+
+// we can avoid zombie state by using wait sys call, SIGCHLD signal, by using handler
+
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/wait.h>
