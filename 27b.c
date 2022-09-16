d48bfab796f8c1363fd19343e1d140662f94e7cb
himanshudigrase <42116677+himanshudigrase@users.noreply.github.com>
2022-09-16 13:53:43 +0530

    Update 27b.c

--- 27b.c
+++ 27b.c
@@ -2,14 +2,18 @@
 // ID: MT2022155
 
 //Question 27b:  Write a program to execute `ls -Rl` by the following system calls 
-    //2. `execvlp`
+    //2. `execlp`
 
 #include<unistd.h>
+
+
+//duplicate the actions of the shell in searching for an executable file if the specified filename does not contain a slash (/) character.
 
 void main(){
     char *execpath = "/bin/ls";
     char *arg1 = "-Rl";
     char *arg2 = "/home/hims";
 
+        
     execlp(execpath,execpath,arg1,arg2,NULL);
 }
